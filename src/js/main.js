d09c7e180e3e51c822717d6d0318ba29c6777ffa
Sergey Greben <greben.sergey1@gmail.com>
2024-08-08 09:00:29 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import './_components.js';
+
+//scripts
+import './scripts/swiper.js';
---
+++ src/js/scripts/swiper.js
@@ -0,0 +1,35 @@
+// Подключение свайпера
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+const hero_swiper = new Swiper('.hero-swiper', {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+    },
+});
+const speciality_swiper = new Swiper('.speciality-swiper', {
+    slidesPerView: 8,
+    spaceBetween: 22,
+    loop: false,
+    navigation: {
+        prevEl: ".speciality-swiper__swiper-button-prev",
+        nextEl: ".speciality-swiper__swiper-button-next",
+    },
+});
+const works_swiper = new Swiper('.works-swiper', {
+    slidesPerView: 4.8,
+    spaceBetween: 8,
+    loop: false,
+    navigation: {
+        prevEl: ".works-swiper__swiper-button-prev",
+        nextEl: ".works-swiper__swiper-button-next",
+    },
+    // pagination: {
+    //     el: ".speciality-swiper__swiper-pagination",
+    //     clickable: true
+    // },
+});
